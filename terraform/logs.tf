0567dd5760cd4f875953340b3cf5f07fcc7b8fc4
John Cameron <john@cameronfoundation.org>
2025-01-09 14:26:47 +1100

    Replace all cb with tc-me-test

--- terraform/logs.tf
+++ terraform/logs.tf
@@ -1,17 +1,17 @@
 # logs.tf
 
 # Set up CloudWatch group and log stream and retain logs for 30 days
-resource "aws_cloudwatch_log_group" "cb_log_group" {
-  name              = "/ecs/cb-app"
+resource "aws_cloudwatch_log_group" "tc-me-test_log_group" {
+  name              = "/ecs/tc-me-test-app"
   retention_in_days = 30
 
   tags = {
-    Name = "cb-log-group"
+    Name = "tc-me-test-log-group"
   }
 }
 
-resource "aws_cloudwatch_log_stream" "cb_log_stream" {
-  name           = "cb-log-stream"
-  log_group_name = aws_cloudwatch_log_group.cb_log_group.name
+resource "aws_cloudwatch_log_stream" "tc-me-test_log_stream" {
+  name           = "tc-me-test-log-stream"
+  log_group_name = aws_cloudwatch_log_group.tc-me-test_log_group.name
 }
 
